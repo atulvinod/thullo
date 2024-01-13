c95c17c87b056a7a94e9fc502361b164be3f1ae6
atulvinod <atulvinod1911@gmail.com>
2024-01-13 13:04:19 +0530

    refactor code

--- backend/migrations/20240113060923_apply-modified-updated-strategy.js
+++ backend/migrations/20240113060923_apply-modified-updated-strategy.js
@@ -12,17 +12,7 @@ function updateColumnModifiedQuery(tablename, db_client = 'mysql') {
     return `ALTER TABLE ${tablename} MODIFY COLUMN modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;`;
 }
 
-async function executeQueryForPostgres(knex) {
-    await knex.raw(`
-            CREATE OR REPLACE FUNCTION update_changetimestamp_column()
-            RETURNS TRIGGER AS $$
-            BEGIN
-            NEW.modified = now(); 
-            RETURN NEW;
-            END;
-            $$ language 'plpgsql';
-        `);
-
+async function executeTableLevelChanges(knex) {
     for (let i = 0; i < TABLE_NAMES.length; i += 1) {
         await knex.raw(
             updateColumnModifiedQuery(
@@ -33,13 +23,21 @@ async function executeQueryForPostgres(knex) {
     }
 }
 
+async function executeQueryForPostgres(knex) {
+    await knex.raw(`
+            CREATE OR REPLACE FUNCTION update_changetimestamp_column()
+            RETURNS TRIGGER AS $$
+            BEGIN
+            NEW.modified = now(); 
+            RETURN NEW;
+            END;
+            $$ language 'plpgsql';
+        `);
+    await executeTableLevelChanges(knex);
+}
+
 async function executeQueryForMySQL(knex) {
-    for (let i = 0; i < TABLE_NAMES.length; i += 1) {
-        await knex.raw(
-            updateColumnModifiedQuery(TABLE_NAMES[i]),
-            process.env.DATABASE_CLIENT,
-        );
-    }
+    await executeTableLevelChanges(knex);
 }
 
 /**
